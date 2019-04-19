2e0660becc3ee54821b1815b6bfe7fe47d0cd02c
Benjamin Milligan <benjaminmilligan@Benjamins-MacBook-Pro-2.local>
2019-04-19 15:29:21 -0500

    added home page

---
+++ frontend/src/common/home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Favicon from 'react-favicon';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Register, Login} from '../user/register.js'
+import {NavBar} from 'navbar.js';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        <Router>
+            <NavBar>
+            </NavBar>
+            
+        </Router>
+        );
+        
+    }
+
+}
