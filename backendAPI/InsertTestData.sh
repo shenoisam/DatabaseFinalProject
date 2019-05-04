319ea6ca66fa41d43ac5968dd99c6a8a9e6de193
Sam Shenoi <Samuel_Shenoi1@baylor.edu>
2019-05-04 15:32:34 -0500

    Fixed err handling stuff and section query

--- backendAPI/InsertTestData.sh
+++ backendAPI/InsertTestData.sh
@@ -5,3 +5,4 @@ echo This script will insert some test data into your Database Final project usi
 curl -d "&CourseName=Test&ID=3321&Year=2019&Semester=Winter&NumStu=20&Comment1=This is a potato&Comment2=This also sucks" -X POST http://localhost:8888/CreateGoals
 #curl -d "&ID=1234&Description=This goal sucks&Curriculum=New Potato" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:8888/CreateGoals
 #curl -d "&ID=3321&Semester=Winter&GoalsID=1234&Year=2019&CourseName=Test" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:8888/CreateSectionGoal
+
--- backendAPI/Routes/Router.js
+++ backendAPI/Routes/Router.js
@@ -252,7 +252,41 @@ router.post('/GetAllCurriculums',Curriclum.GetAllCurriculums,routes.query)
              - otherwise returns an err object {err : "Something went wrong"}
 */
 router.post('/GetGoals',Goals.GetGoals,routes.query)
+
+/*
+  description: This function gets a section
+  parameters: 
+        - ID         - the ID of the section
+        - Semester   - the semester for this section 
+        - Year       - The year that this section was offered
+        - CourseName - The course name associated with this section
+  return: 
+*/
 router.post('/GetSection',Section.GetSection,routes.query)
+
+/*
+  description: This function gets a section
+  parameters: 
+        - Spring     - if we are looking for section with Spring, enter spring otherwise enter ?
+        - Summer     - if we are looking for section with Summer, enter summer otherwise enter ?
+        - Fall       - if we are looking for section with Fall, enter fall otherwise enter ?
+        - Winter     - if we are looking for section with winter, enter spring otherwise enter ?
+        - YearLower  - The lower range of the year we are looking for 
+        - YearUpper  - The upper range of the year we are looking for 
+        - CourseName - The name of the course we are looking for
+  return: 
+*/
+router.post('/GetSectionByCourseName', Section.etSectionByCourseNameYearSemester,routes.query)
+
+/*
+  description: This function gets all sections offered
+  parameters: 
+        - ID         - the ID of the section
+        - Semester   - the semester for this section 
+        - Year       - The year that this section was offered
+        - CourseName - The course name associated with this section
+  return: 
+*/
 router.post('/GetAllSections',Section.GetAllSections,routes.query)
 router.post('/GetTopic',Topics.GetTopic,routes.query)
 router.post('/GetCourseGoals',CourseGoals.GetCourseGoal,routes.query)
